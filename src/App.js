e23cf8051c9541dd079248565554f47cc0cbf7dc
Femi Jimoh <femijimoh10@gmail.com>
2024-12-29 02:20:31 +0100

    added modal and styled form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
-
+import { v4 as uuidv4 } from "uuid";
 function App() {
   const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState([
+    {
+      name: "femi",
+      role: "developer",
+      img: "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=400",
+    },
+    {
+      name: "lase",
+      role: "Girl Friend",
+      img: "https://images.pexels.com/photos/2262810/pexels-photo-2262810.jpeg?auto=compress&cs=tinysrgb&w=400",
+    },
+    {
+      name: "bunmi",
+      role: "Manager",
+      img: "https://images.pexels.com/photos/2613260/pexels-photo-2613260.jpeg?auto=compress&cs=tinysrgb&w=400",
+    },
+    {
+      name: "ten",
+      role: "Intern",
+      img: "https://images.pexels.com/photos/2218786/pexels-photo-2218786.jpeg?auto=compress&cs=tinysrgb&w=400",
+    },
+    {
+      name: "nine",
+      role: "Senior",
+      img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=400",
+    },
+  ]);
   const showEmployees = true;
   return (
     <div className="App ">
@@ -12,67 +39,21 @@ function App() {
           <input
             type="text"
             onChange={(e) => {
-              console.log(e.target.value);
               setRole(e.target.value);
             }}
           />
           <div className="flex flex-wrap justify-center">
-            <Employee
-              name=" femi"
-              role="intern"
-              img="https://images.pexels.com/photos/29852895/pexels-photo-29852895/free-photo-of-professional-corporate-headshot-of-smiling-woman.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
-            <Employee
-              name=" joa"
-              role={role}
-              img="https://images.pexels.com/photos/29852895/pexels-photo-29852895/free-photo-of-professional-corporate-headshot-of-smiling-woman.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
-            <Employee
-              name=" lase"
-              img="https://images.pexels.com/photos/29852895/pexels-photo-29852895/free-photo-of-professional-corporate-headshot-of-smiling-woman.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
-            <Employee
-              name=" femi"
-              role="intern"
-              img="https://images.pexels.com/photos/29852895/pexels-photo-29852895/free-photo-of-professional-corporate-headshot-of-smiling-woman.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
-            <Employee
-              name=" joa"
-              role={role}
-              img="https://images.pexels.com/photos/29852895/pexels-photo-29852895/free-photo-of-professional-corporate-headshot-of-smiling-woman.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
-            <Employee
-              name=" lase"
-              img="https://images.pexels.com/photos/29852895/pexels-photo-29852895/free-photo-of-professional-corporate-headshot-of-smiling-woman.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
-            <Employee
-              name=" femi"
-              role="intern"
-              img="https://images.pexels.com/photos/29852895/pexels-photo-29852895/free-photo-of-professional-corporate-headshot-of-smiling-woman.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
-            <Employee
-              name=" joa"
-              role={role}
-              img="https://images.pexels.com/photos/29852895/pexels-photo-29852895/free-photo-of-professional-corporate-headshot-of-smiling-woman.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
-            <Employee
-              name=" lase"
-              img="https://images.pexels.com/photos/29852895/pexels-photo-29852895/free-photo-of-professional-corporate-headshot-of-smiling-woman.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
-            <Employee
-              name=" femi"
-              role="intern"
-              img="https://images.pexels.com/photos/29852895/pexels-photo-29852895/free-photo-of-professional-corporate-headshot-of-smiling-woman.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
-            <Employee
-              name=" joa"
-              role={role}
-              img="https://images.pexels.com/photos/29852895/pexels-photo-29852895/free-photo-of-professional-corporate-headshot-of-smiling-woman.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
-            <Employee
-              name=" lase"
-              img="https://images.pexels.com/photos/29852895/pexels-photo-29852895/free-photo-of-professional-corporate-headshot-of-smiling-woman.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
+            {employees.map((employee) => {
+              console.log(uuidv4());
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
           </div>
         </>
       ) : (
